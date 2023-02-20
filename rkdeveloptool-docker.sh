aae5b8a1f3e326a9cdd3fc5434f8e02b307c006c
Milas Bowman <milasb@gmail.com>
2023-02-19 22:22:06 -0500

    script robustness improvements

--- rkdeveloptool-docker.sh
+++ rkdeveloptool-docker.sh
@@ -2,13 +2,34 @@
 
 set -e
 
-dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+self=$(
+    self=${0}
+    while [ -L "${self}" ]
+    do
+        cd "${self%/*}"
+        self=$(readlink "${self}")
+    done
+    cd "${self%/*}"
+    echo "$(pwd -P)/${self##*/}"
+)
 
-docker run \
+# the substitution uses :+ to ensure that no empty quoted value is present
+# if no mount flags are being passed, or the run command will interpret the
+# empty literal as the image name
+mnt_args=
+if [ -d /dev/usb ]; then
+  mnt_args="${mnt_args} --mount=type=bind,src=/dev/usb/,dst=/dev/usb/"
+fi
+
+out_dir=$(dirname "${self}")/out
+if [ -d "${out_dir}" ]; then
+  mnt_args="${mnt_args} --mount=type=bind,src=$(dirname "${self}")/out,dst=/out"
+fi
+
+docker --context=default run \
   --rm \
   -it \
   --init \
   --privileged \
-  --mount=type=bind,src=/dev/usb/,dst=/dev/usb/ \
-  --mount="type=bind,src=${dir}/out,dst=/out" \
+  ${mnt_args:+${mnt_args}} \
   ghcr.io/milas/rkdeveloptool "$@"
